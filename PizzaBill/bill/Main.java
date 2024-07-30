9a7a975902125ec3b0d2e0f87b3ec253fb6898cd
Sneha Gupta <156881708+Snehagupta00@users.noreply.github.com>
2024-07-31 00:09:47 +0530

    Add files via upload

---
+++ PizzaBill/bill/Main.java
@@ -0,0 +1,52 @@
+package bill;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("---------------------------------------------Welcome to Pizzamania------------------------------------------");
+        System.out.println(" Which pizza:(1.Veg Pizza   2.Non-Veg Pizza   3.Delux Veg Pizza   4.Delux Non-Veg Pizza) ===>");
+        Scanner sc =new Scanner(System.in);
+        int ch = sc.nextInt();
+
+        switch (ch){
+            case 1:
+                pizza vegPizza = new pizza(true);
+                vegPizza.addExtraToppings();
+                vegPizza.addExtraCheese();
+                vegPizza.takeAway();
+                vegPizza.getBill();
+               break;
+            case 2:
+                pizza nonvegPizza = new pizza(false);
+                nonvegPizza.addExtraToppings();
+                nonvegPizza.addExtraCheese();
+                nonvegPizza.takeAway();
+                nonvegPizza.getBill();
+                break;
+            case 3:
+                DeluxPizza veg = new DeluxPizza(true);
+                veg.basePizzaPrice = 550;
+                veg.addExtraToppings();
+                veg.addExtraCheese();
+                veg.takeAway();
+                break;
+            case 4:
+                DeluxPizza nonveg = new DeluxPizza(false);
+                nonveg.basePizzaPrice = 650;
+                nonveg.addExtraToppings();
+                nonveg.addExtraCheese();
+                nonveg.takeAway();
+                break;
+
+            default:
+                System.out.println("sorry Enter again!!!!!!");
+return;
+        }
+sc.close();
+
+    }
+}
+
